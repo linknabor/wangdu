f906fb50f6e7d01f20f5258fefeb861a5fd698e3
davidhardson <davidhardson@hotmail.com>
2020-03-06 16:10:12 +0800

    SQL修正

--- common/src/main/java/com/yumu/hexie/model/user/UserRepository.java
+++ common/src/main/java/com/yumu/hexie/model/user/UserRepository.java
@@ -32,32 +32,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	 * @return
 	 */
 	
-	
-//	if (currUser.getTotal_bind() <= 0) {//从未绑定过的做新增
-//		currUser.setTotal_bind(1);
-//		currUser.setSect_id(house.getSect_id());
-//		currUser.setSect_name(house.getSect_name());
-//		currUser.setCell_id(house.getMng_cell_id());
-//		currUser.setCell_addr(house.getCell_addr());
-//		
-//		user.setTotal_bind(1);
-//		user.setSect_id(house.getSect_id());
-//		user.setSect_name(house.getSect_name());
-//		user.setCell_id(house.getMng_cell_id());
-//		user.setCell_addr(house.getCell_addr());	//set到session
-//		
-//	}else {
-//		long bind = currUser.getTotal_bind()+1;
-//		user.setTotal_bind(bind);
-//		currUser.setTotal_bind(bind);
-//	}
-//	
-	
 	@Modifying
 	@Transactional
 	@Query(value = "update user set total_bind = ?1, sect_id = ?2, "
-			+ "sect_name = ?3, cell_id = ?4, cell_addr = ?5 office_tel = ?6 "
-			+ " where id = ?10 ", nativeQuery = true)
+			+ " sect_name = ?3, cell_id = ?4, cell_addr = ?5, office_tel = ?6 "
+			+ " where id = ?7 ", nativeQuery = true)
 	public int updateUserByHouse(long totalBind, String sectId, String sectName, 
 			String cellId, String cellAddr, String officeTel, long id);
 
