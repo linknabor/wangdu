100acad966cca7c42abf67c922107638188001bd
davidhardson <davidhardson@hotmail.com>
2020-07-21 10:45:41 +0800

    二维码支付

--- hexie/src/main/java/com/yumu/hexie/web/shequ/WuyeController.java
+++ hexie/src/main/java/com/yumu/hexie/web/shequ/WuyeController.java
@@ -586,4 +586,32 @@ public class WuyeController extends BaseController {
 		return BaseResult.successResult(discountDetail);
 	}
 	
+	/**
+	 * 创建交易，获取预支付ID
+	 * stmtId在快捷支付的时候会用到
+	 * @param user
+	 * @param prepayReq
+	 * @return
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/getPrePayInfo4Qrcode", method = RequestMethod.POST)
+	@ResponseBody
+	public BaseResult<WechatPayInfo> getPrePayInfo4Qrcode(@RequestBody PrepayReqVO prepayReqVo) throws Exception {
+		
+		WechatPayInfo result = new WechatPayInfo();
+		log.info("getPrePayInfo4Qrcode prepayReqVo : " + prepayReqVo);
+		PrepayRequestDTO dto = new PrepayRequestDTO();
+		BeanUtils.copyProperties(prepayReqVo, dto);
+		
+		User user = new User();
+		dto.setUser(user);
+		user.setAppId(prepayReqVo.getAppid());
+		user.setOpenid(prepayReqVo.getOpenid());
+		
+		log.info("getPrePayInfo4Qrcode prepayRequestDTO : " + dto);
+		result = wuyeService.getPrePayInfo(dto);
+		return BaseResult.successResult(result);
+	}
+	
 }
